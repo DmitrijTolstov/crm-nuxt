3d48da9d198c6742a416faac7b2829fa535d850f
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-12-10 14:28:56 +0400

    logging out of the current user session

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,5 +1,23 @@
 <script setup>
 
+import { useAuthStore, isLoading } from '@/store/auth.store'
+import { account } from '@/utils/appwrite';
+
+
+const loading = isLoading()
+const authStore = useAuthStore()
+const router = useRouter()
+
+
+const logout = async () => {
+	loading.set(true)
+	await account.deleteSession('current')
+	authStore.clear()
+	await router.push('/login')
+	loading.set(false)
+}
+
+
 </script>
 <template>
 	<aside class='px-5 py-8 bg-sidebar h-full relative w-full'>
@@ -7,7 +25,7 @@
 			<NuxtImg src='/logo.svg' class='mx-auto' width='100' />
 		</nuxt-link>
 		<button class='absolute top-2 right-3 transition-colors hover:text-primary'>
-			<Icon size='22' name='line-md:logout' />
+			<Icon size='22' name='line-md:logout' @click='logout' />
 		</button>
 		<LayoutMenu class='mt-6' />
 	</aside>
