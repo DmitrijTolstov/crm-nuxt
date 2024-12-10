040fcc43ccc80bf53ab99506f54b40724dfc02ca
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-12-10 13:28:31 +0400

    the login component of the system

---
+++ pages/Login.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+</script>
+<template>
+	<div class='flex items-center justify-center min-h-screen w-full'>
+		<div class='rounded bg-sidebar w-1/4 p-5'>
+			<h1 class='text-2xl font-bold text-center mb-5'>Login</h1>
+			<form>
+				<UiInput v-model='email' placeholder='email' type='email' class='mb-3' />
+				<UiInput v-model='password' placeholder='password' type='password' class='mb-3' />
+				<UiInput v-model='name' placeholder='name' type='text' class='mb-3' />
+			</form>
+			<div class="flex items-center justify-center gap-5">
+				<UiButton type='button' @click='login'> Login</UiButton>
+				<UiButton type='button' @click='register'> Register</UiButton>
+			</div>
+		</div>
+
+	</div>
+</template>
+<style scoped></style>
