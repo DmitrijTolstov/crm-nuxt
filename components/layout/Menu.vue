555d388cd79f8cfcd2eda7291fdccf3d8fe7f2ca
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-12-10 13:30:28 +0400

    the menu component and its data

---
+++ components/layout/Menu.vue
@@ -0,0 +1,16 @@
+<script lang='ts' setup>
+import { MENU_DATA } from './menu.data';
+
+</script>
+<template>
+	<div>
+		<nuxt-link v-for='{ name, icon, url } in MENU_DATA' :key='name' :to='url' class='flex items-center py-1 px-3 rounded-lg w-full
+		hover:bg-gray-700 hover:shadow transition-all ms-2.5'>
+			<Icon :name='icon' class='mr-3'>
+			</Icon>
+			<span>{{ name }}</span>
+		</nuxt-link>
+
+	</div>
+</template>
+<style scoped></style>
