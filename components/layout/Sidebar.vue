e7c3d2e8a9ef78b70d3f1180e49ca3cf58270dad
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-12-10 13:24:44 +0400

    sidebar component

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,6 +1,15 @@
 <script setup>
+
 </script>
 <template>
-
+	<aside class='px-5 py-8 bg-sidebar h-full relative w-full'>
+		<nuxt-link to="/">
+			<NuxtImg src='/logo.svg' class='mx-auto' width='100' />
+		</nuxt-link>
+		<button class='absolute top-2 right-3 transition-colors hover:text-primary'>
+			<Icon size='22' name='line-md:logout' />
+		</button>
+		<LayoutMenu class='mt-6' />
+	</aside>
 </template>
 <style scoped></style>
