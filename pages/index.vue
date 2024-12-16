475fcc673fe17a277e5e20335a48120b42e6632d
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-12-16 11:39:23 +0400

    the logic of dragging the card and updating from the server

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,51 @@ import type { ICard, IColumn } from '~/components/kanban/kanban.types';
 import { useKanbanQuery } from '~/components/kanban/UseKanbanQuery';
 import dayjs from 'dayjs'
 import CreateDeal from '~/components/kanban/CreateDeal.vue';
+import type { EnumStatus } from '~/types/deals.types';
+import { useMutation } from '@tanstack/vue-query';
+import { COLLECTION_DEALS, DB_ID } from '~/app.constants';
 
 useSeoMeta({
 	title: 'Home | CRM System'
 })
 
 
-const dragCardRef = ref<ICard | null>
-const dsourceColumnRef = ref<IColumn | null>
+const dragCardRef = ref<ICard | null>()
+const sourceColumnRef = ref<IColumn | null>()
 
 const { data, isLoading, refetch } = useKanbanQuery()
+
+
+type TMutationVariables = {
+	docId: string,
+	status: EnumStatus
+}
+
+
+const { mutate } = useMutation({
+	mutationKey: ['move card'],
+	mutationFn: ({ docId, status }: TMutationVariables) => DB.updateDocument(DB_ID, COLLECTION_DEALS, docId, {
+		status
+	}),
+	onSuccess() {
+		refetch()
+	},
+})
+
+function handleCardStart(card: ICard, column: IColumn) {
+
+	dragCardRef.value = card
+	sourceColumnRef.value = column
+
+}
+
+function handleDrop(targetColumn: IColumn) {
+
+	if (dragCardRef.value && sourceColumnRef.value) {
+		mutate({ docId: dragCardRef.value.id, status: targetColumn.id })
+	}
+}
+
 </script>
 <template>
 	<div class="p-10">
@@ -20,13 +55,14 @@ const { data, isLoading, refetch } = useKanbanQuery()
 		<div v-if='isLoading'>Loading...</div>
 		<div v-else>
 			<div class='grid grid-cols-5 gap-16'>
-				<div v-for='(column) in data'>
+				<div v-for='(column) in data' @dragover.prevent @drop='handleDrop(column)'>
 					<div class='rounded bg-slate-700 py-1 px-5 mb-2 text-center text-nowrap'>
 						{{ column.name }}
 					</div>
 					<CreateDeal :refetch='refetch' :status='column.id' />
 					<div>
-						<UiCard v-for='card in column.items ' class='mb-3' draggable='true'>
+						<UiCard draggable='true' @dragstart='handleCardStart(card, column)'
+							v-for='card in column.items ' class='mb-3'>
 							<UiCardHeader role='button'>
 								<UiCardTitle>{{ card.name }}</UiCardTitle>
 
